0b62b3cc349cf0acf7978ca69acf59034228dbcc
DucLV08 <duc080999@gmail.com>
2023-03-08 00:10:29 +0700

    update api home

--- src/playlists/serializers.py
+++ src/playlists/serializers.py
@@ -11,12 +11,19 @@ class TopicPlaylistSerializers(serializers.ModelSerializer):
 class PlaylistSerializers(serializers.ModelSerializer):
     class Meta:
         model = Playlists
-        fields = ["id", "title", "thumbnail", "isoffical", "is_indie", "release_date", "sort_description", "released_at",
-                    "pr", "artist_names", "play_item_mode", "sub_type", "thumbnail_m", "is_shuffle", "is_private",
-                    "user_name", "is_album", "text_type", "is_single"]
+        fields = ["id", "title", "thumbnail", "isoffical", "is_indie", "release_date", "sort_description", "released_at"
+            , "pr", "artist_names", "play_item_mode", "sub_type", "thumbnail_m", "is_shuffle", "is_private",
+                  "user_name", "is_album", "text_type", "is_single"]
+
+
+class PlaylistSortDataSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = Playlists
+        fields = ["id", "thumbnail", "thumbnail_m", "title", "sort_description", "artist_names"]
 
 
 class ArtistSerializers(serializers.ModelSerializer):
     class Meta:
         model = Artists
-        fields = ["id", "name", "spotlight", "alias", "thumbnail", "thumbnail_m", "is_oa", "is_oa_brand", "total_follow"]
+        fields = ["id", "name", "spotlight", "alias", "thumbnail", "thumbnail_m", "is_oa", "is_oa_brand",
+                  "total_follow"]
