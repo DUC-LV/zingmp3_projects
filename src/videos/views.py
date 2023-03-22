61ef59b4062d99fd01627b62e42337a5f2dce759
DucLV08 <duc080999@gmail.com>
2023-03-22 21:02:11 +0700

    api category video

--- src/videos/views.py
+++ src/videos/views.py
@@ -55,10 +55,19 @@ class VideoAPIView(APIView):
 
         return JsonResponse(serializer, safe=False)
 
+    def get(self, request):
+        video = Videos.objects.all()
+        if not video.exists():
+            return HttpResponse(status=404)
+
+        serializer = VideoSerializers(video, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
 
 class CategoryVideoAPIView(APIView):
     def get(self, request, id):
         items = []
+        # topicVideo *******
         data_topic = []
         all_topic = TopicVideo.objects.all()
         for all_tp in all_topic:
@@ -70,18 +79,34 @@ class CategoryVideoAPIView(APIView):
             "display": 0,
             "items": data_topic
         }
+        items.append(dict_topic_video)
+        # listVideo ******
         topic_video = TopicVideo.objects.filter(id=id)
+        print(topic_video)
         video_topic = VideoOfTopic.objects.filter(topic_id=topic_video[0])
+        video_res = []
         for vd_tp in video_topic:
             video_artist = ArtistOfVideo.objects.filter(video_id=vd_tp.video)
             artist_data = []
+            artist = {}
             for artist in video_artist:
                 artist_data.append(ArtistSerializers(artist.artist).data)
-        dict_res = dict(dict_topic_video)
-        items.append(dict_res)
+                artist = ArtistSerializers(artist.artist).data
+            video_data = VideoSerializers(vd_tp.video).data
+            video_dict = dict(video_data, **{"artists": artist_data}, **{"artist": artist})
+            video_res.append(video_dict)
+        dict_video = {
+            "id": "list_vod",
+            "name": "list_vod",
+            "type": "LIST",
+            "display": 0,
+            "items": video_res
+        }
+        items.append(dict_video)
         res = {
             "err": 0,
             "msg": "Success",
+            "parent": TopicVideoSerializers(topic_video[0]).data,
             "data": items
         }
         return JsonResponse(res, safe=False)
