027967dee48a77c03c331a4d1a54e591ee925a33
DucLV08 <duc080999@gmail.com>
2023-03-24 19:45:01 +0700

    create models topic_category

--- src/src/settings.py
+++ src/src/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'artists',
     'album',
     'videos',
+    'topic_category',
 ]
 
 MIDDLEWARE = [
--- src/topic_category/admin.py
+++ src/topic_category/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import TopicHub, Hub, PlaylistOfHub, HubOfTopic
+
 
 # Register your models here.
+@admin.register(TopicHub)
+class TopicHub(admin.ModelAdmin):
+    list_display = ["title", "created_at"]
+
+
+admin.register(TopicHub)
+
+
+@admin.register(Hub)
+class Hub(admin.ModelAdmin):
+    list_display = ["cover", "thumbnail", "thumbnail_has_text", "thumbnail_r", "title", "description", "created_at"]
+
+
+admin.register(Hub)
+
+
+@admin.register(HubOfTopic)
+class HubOfTopic(admin.ModelAdmin):
+    list_display = ["topic", "hub", "created_at"]
+
+
+admin.register(HubOfTopic)
+
+
+@admin.register(PlaylistOfHub)
+class PlaylistOfHub(admin.ModelAdmin):
+    list_display = ["hub", "playlist", "created_at"]
+
+
+admin.register(PlaylistOfHub)
