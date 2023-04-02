5005622a0d4724e9fe63db4876073b89372da433
DucLV08 <duc080999@gmail.com>
2023-04-02 18:13:27 +0700

    config

--- src/front-end/music-app/src/container/Layout.tsx
+++ src/front-end/music-app/src/container/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Box } from "theme-ui";
 import Header from "../components/Header";
 import SearchBar from "./SearchBar";
@@ -6,16 +6,19 @@ import axios from "axios";
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => {
 	const checkout = typeof window !== 'undefined' ? localStorage.getItem('access_token') : undefined;
+	// const [data, setData] = useState();
 	// const url = 'http://localhost:8000/user-info/';
 	// // eslint-disable-next-line react-hooks/exhaustive-deps
 	// const config : object = {
-	// 	'Authorization': typeof window !== 'undefined' ? 'Bearer ' + localStorage.getItem('access_token'): '',
-	// 	'Content-Type': 'application/json',
-	// 	'accept': 'application/json',
+	// 	headers: {
+	// 		'Authorization': typeof window !== 'undefined' ? 'Bearer ' + localStorage.getItem('access_token'): '',
+	// 		'Content-Type': 'application/json',
+	// 		'accept': 'application/json',
+	// 	}
 	// }
 	// useEffect(() => {
 	// 	axios.get(url, config).then(res => {
-	// 		console.log(res)
+	// 		setData(res.data.data);
 	// 	})
 	// }, [config])
 	return(
