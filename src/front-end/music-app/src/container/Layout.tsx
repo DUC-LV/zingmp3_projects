51e52b623c65c5fe3a1593d6d39d051bc7b3bfa3
DucLV08 <duc080999@gmail.com>
2023-04-10 21:34:30 +0700

    config call api user-info

--- src/front-end/music-app/src/container/Layout.tsx
+++ src/front-end/music-app/src/container/Layout.tsx
@@ -3,24 +3,16 @@ import { Box } from "theme-ui";
 import Header from "../components/Header";
 import SearchBar from "./SearchBar";
 import axios from "axios";
+import axiosInstance from "../services/axiosInstance";
 
 const Layout = ({ children }: React.PropsWithChildren<{}>) => {
 	const checkout = typeof window !== 'undefined' ? localStorage.getItem('access_token') : undefined;
 	const [data, setData] = useState();
-	const url = 'http://localhost:8000/user-info/';
-	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const config : object = {
-		headers: {
-			'Authorization': typeof window !== 'undefined' ? 'Bearer ' + localStorage.getItem('access_token'): '',
-			'Content-Type': 'application/json',
-			'accept': 'application/json',
-		}
-	}
 	useEffect(() => {
 		if(!localStorage.getItem('access_token')){
 			return;
 		}
-		axios.get(url, config).then(res => {
+		axiosInstance.get('user-info/').then(res => {
 			setData(res.data.data);
 		})
 	}, [])
